dd4cfc70c225d68be975a1c8653e2b73fcd365c8
kunalrukade <rukade.kunal@gmail.com>
2022-05-21 19:43:07 +0530

    the firstcode to be uploaded ever on git

---
+++ go-demo-1/mascot/mascot.go
@@ -0,0 +1,6 @@
+package mascot
+
+//This function will return the best mascot in the world
+func bestMascot() string {
+	return "Tux"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kunalrukade/Gocourse/go-demo-1
+
+go 1.17
