be27b353e33f2920499353dddf0ea9af8a62c73b
kunalrukade <rukade.kunal@gmail.com>
2022-05-22 08:18:02 +0530

    uploading banger file

---
+++ banger/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	msg := os.Args[1]
+	l := len(msg)
+	s := strings.Repeat("!", l)+ msg + strings.Repeat("!", l)
+	s = strings.ToUpper(s)
+	fmt.Println(s)
+
+
+}
