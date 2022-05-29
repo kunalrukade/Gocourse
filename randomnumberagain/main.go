f9de35feeb2d62001598107cb7fc48ed47458866
kunalrukade <rukade.kunal@gmail.com>
2022-05-30 00:24:27 +0530

    random number tested

--- randomnumberagain/main.go
+++ randomnumberagain/main.go
@@ -1,10 +1,35 @@
 package main
 
-func main() {
-	guess := 10
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-	for n := 0; n != guess {
-		n = rand.Intn(guess + 1)
-		fmt.Printf("%d", n)
+const maxTurns = 5
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+	args := os.Args[1:]
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	for turn := 0; turn < maxTurns; turn++ {
+		n := rand.Intn(guess + 1)
+		//fmt.Printf("%d", n)
+
+		if n == guess {
+			fmt.Println("You win Tan ta da!!!!!!!!!!!!!!!!!!!!!")
+			return
+		}
+	}
+
+	fmt.Println("You Lost, Try again!!!!!!!!!!!!!!!!")
 }
